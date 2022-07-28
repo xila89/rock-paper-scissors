f998e242160f54c6b1fe831ebf5216d872a0580b
Xila89 <ctreinauer@gmail.com>
2022-07-28 16:41:34 -0600

    Update playerSelect

--- code.js
+++ code.js
@@ -3,38 +3,68 @@
 
 //Get Random number 0-4 (for each choice)//
 
+
 //Get Computer Choice 0-4//
 
-window.onload = function getComputerChoice () {
+function getComputerChoice () {
 
-    let randomNumber = Math.floor(Math.random() *5);
+    let randomNumber = Math.floor(Math.random() * 5);
 
-//assign strings to number//
-switch (randomNumber) {
-    case 0: 
-        alert("Rock");
-        break;
-    case 1:
-        alert("Paper");
-        break;
-    case 2:
-        alert("Scissors");
-        break;
-    case 3: 
-        alert("Lizard");
-        break;
-    case 4: 
-        alert("Spock");
-        break;
+    //assign strings to number//
+    switch (randomNumber) {
+        case 0:
+            console.log("Rock");
+            break;
+        case 1:
+            console.log("Paper");
+            break;
+        case 2:
+            console.log("Scissors");
+            break;
+        case 3:
+            console.log("Lizard");
+            break;
+        case 4:
+            console.log("Spock");
+            break;
 
+    }
+    return "Computer selected " + randomNumber;
 }
-console.log(randomNumber); }
-
-
-
 //Prompt User for weapon choice//
+function playRound () {
+
+    let weapon = prompt ("What weapon do you choose?");
+    switch (weapon) {
+        case "rock", 0:
+        break;
+        case "paper", 1:
+        break;
+        case "scissors", 2:
+            break;
+        case "lizard", 3:
+            break;
+        case "spock", 4:
+            break;
+    }
+
+            return "You chose " + weapon;
+    }
+
+const playerSelection = "rock";
+const computerSelection  = getComputerChoice;
+console.log(playRound(playerSelection, computerSelection));
 
 //game rules//
+
+
+
+
+
+
+
+
+
 
 //In the event of a tie//
 
