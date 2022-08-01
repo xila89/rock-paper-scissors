33bf1117ea3a21a7140560dc0f2b7517307a71f6
Xila89 <ctreinauer@gmail.com>
2022-07-31 18:16:42 -0600

    Update to play one round of RPSLS

--- code.js
+++ code.js
@@ -1,50 +1,32 @@
 
 
-let playerScore = 0;
-let computerScore = 0;
 
+    window.onload = function playRound() {
+        let userChoice = prompt("What weapon do you choose?").toLowerCase(); //prompt user for weapon//
+        const weaponChoices =  ["rock", "paper", "scissors", "lizard", "spock"];
+        const randomNumber = Math.floor(Math.random() * 5);
+        const computerChoice = weaponChoices[randomNumber]; 
+        
+    console.log(`You chose ${userChoice} and the computer chose ${computerChoice}`); 
 
-//prompt for user choice//
-window.onload = function playRound() {
-let userChoice = prompt("What weapon will you choose?");
-userChoice = userChoice.toLowerCase();
-
-//create array for weapons and get computer choice//
-    const weaponChoices =  ["rock", "paper", "scissors", "lizard", "spock"];
-    const randomNumber = Math.floor(Math.random() * 5);
-    let computerChoice = weaponChoices[randomNumber]; 
-
-    console.log (`You chose ${userChoice} and the computer chose ${computerChoice}`); 
-//output of both choices//
-
-//create an array with game rules and outcomes//
-const results = [
-    ["t","u","c","c","u"],
-    ["c","t","u","u","c"],
-    ["u","c","t","c","u"],
-    ["c","c","u","t","u"],
-    ["c","u","c","u","t"],
-];
-
-let playerChoiceIndex = weaponChoices.indexOf(userChoice);
-let userResult = results[randomNumber][playerChoiceIndex];
-
-//round results/
-const resultIndex = {
-    "t": "It's a tie! Try again.",
-    "u": "You win! Yaaay!",
-    "c": "The computer wins. Better luck next time.", 
-};
-
-console.log(resultIndex[userResult] +playerScore, +computerScore);
-}
-
-function playGame (playRound) {
-
-}
-//repeat game 5x//
-
-
-
-
+    const results = [
+        ["t","u","c","c","u"],
+        ["c","t","u","u","c"],
+        ["u","c","t","c","u"],
+        ["c","c","u","t","u"],
+        ["c","u","c","u","t"],
+    ];
+    
+    let playerChoiceIndex = weaponChoices.indexOf(userChoice);
+    let userResult = results[randomNumber][playerChoiceIndex];
+    
+    //round results/
+    const resultIndex = {
+        "t": "It's a tie! Try again.",
+        "u": "You win! Yaaay!",
+        "c": "The computer wins. Better luck next time.", 
+    };
+    console.log(resultIndex[userResult]);
+ 
+} 
 
